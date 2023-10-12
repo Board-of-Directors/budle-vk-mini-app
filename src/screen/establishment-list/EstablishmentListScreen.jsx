5c0dc426376350c82475d27210ade2a2a63c7fd6
tretiakovv <55498236+Tretiakovv@users.noreply.github.com>
2023-10-13 00:33:03 +0700

    establishment-list-dev / initial commit: added Vite and React Router

---
+++ src/screen/establishment-list/EstablishmentListScreen.jsx
@@ -0,0 +1,11 @@
+import style from "./EstablishmentScreen.module.css"
+
+const EstablishmentListScreen = () => {
+    return (
+        <div className={style.wrapper}>
+            Список заведений
+        </div>
+    )
+}
+
+export default EstablishmentListScreen
